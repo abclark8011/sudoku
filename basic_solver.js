1cd88f3e3fea8927f6237fe2fe0891bb90c02bb1
Bob Clark <bclark8011@gmail.com>
2018-01-24 12:02:47 -0700

    Added a higher leve Sudoku class and moved some methods out of the Puzzle class into it.  Also implemented an exhaustive search flag to exhaustively find all solutions. Even on the "hardest" puzzle, looking for all solutions only takes a second or so, so I defaulted it to true.

--- basic_solver.js
+++ basic_solver.js
@@ -4,15 +4,69 @@
 // The puzzle is filled out with correct values and returned back.
 
 
-var solutionList = [];
+class Sudoku {
+  constructor(puzzle) {
+    this.puzzle = new Puzzle(puzzle);
+    this._solution = [];
+    this.exhaustive = true;
+  }
+
+  solutionCount() {
+    return this._solution.length;
+  }
+
+  getSolution(idx = 0) {
+    return this._solution[idx];
+  }
+
+  addSolution() {
+    this._solution.push(new Puzzle(this.puzzle._puzzle));
+  }
+
+  set exhaustive(value) {
+    if (typeof(value) != typeof(true)) {
+      throw 'expected boolean value';
+    }
+    this._exhaustive = value;
+  }
+
+  get exhaustive() {
+    return this._exhaustive;
+  }
+
+  // The solver.  Recursively tries all possible solutions.
+  solve(row = 0, col = 0) {
+    if (row == this.puzzle.ROWS) {
+      this.addSolution();
+      return true;
+    }
+
+    let nextRow = this.puzzle.nextRow(row, col),
+        nextCol = this.puzzle.nextCol(row, col);
+
+    // If there's a value, then it came from the inital puzzle.  Skip it.
+    if (this.puzzle.getValue(row, col)) {
+      return this.solve(nextRow, nextCol);
+    }
+
+    // Try all 9 possible values
+    for (let value = 1; value <= 9; value++) {
+      if (this.puzzle.isValid(row, col, value)) {
+        this.puzzle.setValue(row, col, value);
+        if (this.solve(nextRow, nextCol)) {
+          if (!this.exhaustive) return true;
+        }
+      }
+    }
+    this.puzzle.setValue(row, col, 0); // Restore original value
+    return false;
+  }
+};
+
 
 class Puzzle {
-  constructor(puzzle = [[]], copy = false) {
-    if (!copy) {
-      this.puzzle = puzzle;
-    } else {
-      this,puzzle = puzzle.slice();
-    }
+  constructor(puzzle = [[]]) {
+    this.puzzle = puzzle;
   }
 
   get puzzle() {
@@ -20,7 +74,10 @@ class Puzzle {
   }
 
   set puzzle(data) {
-    this._puzzle = data;
+    this._puzzle = [data.length];
+    for (let i = 0; i < data.length; i++) {
+      this._puzzle[i] = data[i].slice();
+    }
     this.ROWS = this.puzzle.length;
     this.COLS = this.puzzle[0].length;
   }
@@ -72,31 +129,7 @@ class Puzzle {
       }
       console.log("");
     }
-  }
-
-  // The solver.  Recursively tries all possible solutions.
-  solve(row = 0, col = 0) {
-    if (row == this.ROWS) {
-      solutionList.push(new Puzzle(this.puzzle));
-      return true;
-    }
-
-    // If there's a value, then it came from the inital puzzle.  Skip it.
-    if (this.getValue(row, col)) {
-      return this.solve(this.nextRow(row, col), this.nextCol(row, col));
-    }
-
-    // Try all 9 possible values
-    for (let value = 1; value <= 9; value++) {
-      if (this.isValid(row, col, value)) {
-        this.setValue(row, col, value);
-        if (this.solve(this.nextRow(row, col), this.nextCol(row, col))) {
-          return true;
-        }
-      }
-    }
-    this.setValue(row, col, 0); // Restore original value
-    return false;
+    console.log("");
   }
 }
 
@@ -113,7 +146,31 @@ var puzzle = [
     [ 0, 0, 8, 5, 0, 0, 0, 1, 0 ],
     [ 0, 9, 0, 0, 0, 0, 4, 0, 0 ]
 ];
+var puzzle2Solutions = [
+    // A puzzle with 2 solutions
+    [ 9, 0, 6, 0, 7, 0, 4, 0, 3 ],
+    [ 0, 0, 0, 4, 0, 0, 2, 0, 0 ],
+    [ 0, 7, 0, 0, 2, 3, 0, 1, 0 ],
+    [ 5, 0, 0, 0, 0, 0, 1, 0, 0 ],
+    [ 0, 4, 0, 2, 0, 8, 0, 6, 0 ],
+    [ 0, 0, 3, 0, 0, 0, 0, 0, 5 ],
+    [ 0, 3, 0, 7, 0, 0, 0, 5, 0 ],
+    [ 0, 0, 7, 0, 0, 5, 0, 0, 0 ],
+    [ 4, 0, 5, 0, 1, 0, 7, 0, 8 ]
+];
 
+var puzzle1Sol = [
+    // Solution to the hardest puzzle
+    [ 8, 1, 2, 7, 5, 3, 6, 4, 9 ],
+    [ 9, 4, 3, 6, 8, 2, 1, 7, 5 ], 
+    [ 6, 7, 5, 4, 9, 1, 2, 8, 3 ], 
+    [ 1, 5, 4, 2, 3, 7, 8, 9, 6 ], 
+    [ 3, 6, 9, 8, 4, 5, 7, 2, 1 ], 
+    [ 2, 8, 7, 1, 6, 9, 5, 3, 4 ], 
+    [ 5, 2, 1, 9, 7, 4, 3, 6, 8 ], 
+    [ 4, 3, 8, 5, 2, 6, 9, 1, 7 ], 
+    [ 7, 9, 6, 3, 1, 8, 4, 5, 0 ] 
+];
 /*
 var puzzle = [
     // Solution to the hardest puzzle
@@ -125,16 +182,18 @@ var puzzle = [
     [ 2, 8, 7, 1, 6, 9, 5, 3, 4 ], 
     [ 5, 2, 1, 9, 7, 4, 3, 6, 8 ], 
     [ 4, 3, 8, 5, 2, 6, 9, 1, 7 ], 
-    [ 7, 9, 6, 3, 1, 8, 4, 5, 2 ],
-    [ 7, 9, 6, 3, 1, 8, 4, 5, 2 ] 
+    [ 7, 9, 6, 3, 1, 8, 4, 5, 2 ]
 ];
 */
 
-var p = new Puzzle(puzzle);
+var sudoku = new Sudoku(puzzle2Solutions);
 
-if (p.solve()) {  
+sudoku.solve();
+if (sudoku.solutionCount() > 0) {  
   console.log("Solution:");
-  solutionList[0].print();
+  for (let i = 0; i < sudoku.solutionCount(); i++) {
+    sudoku.getSolution(i).print();
+  }
 } else {
   console.log("Unsolvable!");
 }
